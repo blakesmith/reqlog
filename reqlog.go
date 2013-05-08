0c4877025ed4b0e1d7646b54e1756ea87626ba43
Blake Smith <blakesmith0@gmail.com>
2013-05-08 12:37:14 -0500

    Add StatHat backend

--- reqlog.go
+++ reqlog.go
@@ -4,23 +4,70 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/stathat/go"
 )
 
 type reqlogHandler struct {
 	handler http.Handler
-	logger  *log.Logger
+	env string
+	backends []Backend
 }
 
-func NewHandler(handler http.Handler, logger *log.Logger) http.Handler {
+type Backend interface {
+	RecordRequest(*http.Request, time.Duration, string)
+}
+
+type Logger struct {
+	logger *log.Logger
+}
+
+type StatHat struct {
+	ezkey string
+}
+
+func NewLogger(l *log.Logger) *Logger {
+	return &Logger{
+		logger: l,
+	}
+}
+
+func NewStatHat(ezkey string) *StatHat {
+	return &StatHat{
+		ezkey: ezkey,
+	}
+}
+
+func NewHandler(handler http.Handler, env string, backends ...Backend) http.Handler {
 	return &reqlogHandler{
 		handler: handler,
-		logger: logger,
+		env: env,
+		backends: backends,
 	}
+}
+
+func (l *Logger) RecordRequest(r *http.Request, duration time.Duration, env string) {
+	l.logger.Printf("(%s) %s %s [%s]", env, r.Method, r.URL, duration)
+}
+
+func (s *StatHat) requestCount(r *http.Request, env string) {
+	stathat.PostEZCount(env+": site request count", s.ezkey, 1)
+}
+
+func (s *StatHat) requestDuration(r *http.Request, duration time.Duration, env string) {
+	stathat.PostEZValue(env+": site request duration ms", s.ezkey, float64(duration*time.Millisecond))
+}
+
+func (s *StatHat) RecordRequest(r *http.Request, duration time.Duration, env string) {
+	go s.requestCount(r, env)
+	go s.requestDuration(r, duration, env)
 }
 
 func (h *reqlogHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 	h.handler.ServeHTTP(w, r)
-	end := time.Now()
-	h.logger.Printf("[request] %s %s [%s]", r.Method, r.URL, end.Sub(start))
+	dur := time.Now().Sub(start)
+	for _, backend := range h.backends {
+		go backend.RecordRequest(r, dur, h.env)
+	}
 }
